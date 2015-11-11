1d85716f96cec8490548ccda47c671e42a737a78
mia81514 <mia81514@gmail.com>
2015-11-11 23:00:54 +0800

    current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    login_key = cookies[:login_key].to_s
+    if login_key.present?
+      user = CacheManager.get_current_user(login_key)
+      cookies[:login_key] = {:value => login_key, :expires => 1.day.from_now}
+    end
+    logger.formatter.params = params
+    logger.formatter.user_id = (user ? user.id : nil)
+    return user
+  end
 end
