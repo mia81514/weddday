bfdd1e10da4eff97142e9dccb3cefcda74d973a1
chester <lsortnj@gmail.com>
2015-11-14 10:20:24 +0800

    refactor

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -1,2 +1,16 @@
 class Questionnaire < ActiveRecord::Base
+  belongs_to :event
+  has_many :questionnaire_questions
+  has_many :guest_replies
+  has_many :guest_groups
+
+  #女方親友
+  def bride_friends
+    return guest_replies.includes(:guest_group).select{|reply| reply.guest_group.is_bride}
+  end
+
+  #男方親友
+  def bridegroom_friends
+    return guest_replies.includes(:guest_group).select{|reply| not reply.guest_group.is_bride}
+  end
 end
--- app/models/questionnaire_question.rb
+++ app/models/questionnaire_question.rb
@@ -1,2 +1,5 @@
 class QuestionnaireQuestion < ActiveRecord::Base
+  TYPE_FILL_IN          = 1
+  TYPE_SINGLE_CHOICE    = 2
+  TYPE_MULTIPLE_CHOICE  = 3
 end
