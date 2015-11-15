a060670880e286205cf846fb20318dd060bea497
mia81514 <mia81514@gmail.com>
2015-11-15 15:14:45 +0800

    可噴error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,4 +22,12 @@ class ApplicationController < ActionController::Base
   def redirect_to_root
     redirect_to root_path
   end
+
+  def error(data)
+    render(:status => "error", data)
+  end
+
+  def error_text(msg'')
+    render(:text=>{:error=>msg}.to_json)
+  end
 end
